4748673d91fed81f2aedccb1b58dff587f8faa59
aadityakalwani <aadityakalwani@gmail.com>
2021-08-07 13:26:17 +0100

    made the name and date custom, but need to solve some dict issue

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 # first line
 from datetime import datetime
 
-PYCON_DATE = datetime(year=2021, month=9, day=11, hour=0)
-countdown = PYCON_DATE - datetime.now()
-print(f"Countdown to my birthday: {countdown}")
+event_name = input("Enter the name of this event: ")
+
+d = {}
+d["year"] = int(input("Enter the year: "))
+d["month"] = int(input("Enter the month: "))
+d["day"] = int(input("Enter the day: "))
+
+target_date = datetime(year=d["year"], month=d["month"], day=d["day"])
+countdown = target_date - datetime.now()
+print(f"Countdown to {event_name}: {countdown}")
