d1b4207f4e81b7369484ef71fe03517446b127a7
aadityakalwani <aadityakalwani@gmail.com>
2021-08-07 13:15:19 +0100

    first code done, countdown to my birthday

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 # first line
+from datetime import datetime
+
+PYCON_DATE = datetime(year=2021, month=9, day=11, hour=0)
+countdown = PYCON_DATE - datetime.now()
+print(f"Countdown to my birthday: {countdown}")
