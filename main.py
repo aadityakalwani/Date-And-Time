ef7028674790c84ddfe43185af15ccdb24bbe8b2
aadityakalwani <aadityakalwani@gmail.com>
2021-08-07 13:36:43 +0100

    solved the dict issue and formatted a bit better. now its a date-countdown-er

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from datetime import datetime
 event_name = input("Enter the name of this event: ")
 
 d = {}
-d["year"] = int(input("Enter the year: "))
-d["month"] = int(input("Enter the month: "))
-d["day"] = int(input("Enter the day: "))
+d.update({"year": int(input("Enter the year: "))})
+d.update({"month": int(input("Enter the month: "))})
+d.update({"day": int(input("Enter the day: "))})
 
 target_date = datetime(year=d["year"], month=d["month"], day=d["day"])
 countdown = target_date - datetime.now()
